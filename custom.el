fa2fa8b22c590a1ed01780f5a88e63a5fbcc7e70
Ayush Jha <ayush@wasmer.io>
2023-08-26 15:29:46 +0545

    added custom.el

---
+++ custom.el
@@ -0,0 +1,105 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline success warning error])
+ '(ansi-color-names-vector
+   ["gray35" "#a60000" "#005e00" "#813e00" "#0031a9" "#721045" "#00538b" "gray65"])
+ '(awesome-tray-mode-line-active-color "#0031a9")
+ '(awesome-tray-mode-line-inactive-color "#d7d7d7")
+ '(blink-cursor-mode t)
+ '(centered-window-mode nil)
+ '(column-number-mode t)
+ '(custom-safe-themes
+   '("eb50f36ed5141c3f702f59baa1968494dc8e9bd22ed99d2aaa536c613c8782db" "4320a92406c5015e8cba1e581a88f058765f7400cf5d885a3aa9b7b9fc448fa7" "d0f70691dd3c066f8030e32ff842550365c0444f0203fbbdb2dd3a4ea47ffba4" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "a138ec18a6b926ea9d66e61aac28f5ce99739cf38566876dc31e29ec8757f6e2" "ce4234c32262924c1d2f43e6b61312634938777071f1129c7cde3ebd4a3028da" "016f665c0dd5f76f8404124482a0b13a573d17e92ff4eb36a66b409f4d1da410" "4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" "e963debb6066f1b4a6bdd24b2f4bf0ba4496c5ab7ecced9debfd4d1a512d94d1" "dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "500244baf96301754cf9f388e33115405b9d75f561dd535c0a26f97a5eab52c8" "8b930a6af47e826c12be96de5c28f1d142dccab1927f196589dafffad0fc9652" "4bf6e7eb89e40ff5ae016bfc8178acc98d59de12d142191b370f551da24d77ac" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "a589c43f8dd8761075a2d6b8d069fc985660e731ae26f6eddef7068fece8a414" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee" "ae426fc51c58ade49774264c17e666ea7f681d8cae62570630539be3d06fd964" "dc11cee30927281fe3f5c77372119d639e77e86aa794dce2a6ff019afdfbec9e" "f4cdc8dea941e3c7e92b907e62cdc03e0483a350b738a43c2e118ce6be9880a6" "d7d317a4a3629d91129493ed6caa795c38e3b120da4df0156aa7700e9fe7d88c" "bdf2b5b617ea284bf2635f28509b3a5ce3fb1ed82cac00196a71cd8403fa3e60" "830596655dc39879096d9b7772768de6042fb5a4293c6b90c98a9b98bce96e4a" "e33efadcb0406fc9b0f0fd78e3afbfdb2825d9a9796b8d6671fad6bf38427bd1" "d4a02820fada527bad6414f1ba432d87155fa627f3111a226018885d3127f093" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "e27c391095dcee30face81de5c8354afb2fbe69143e1129109a16d17871fc055" "88deeaaab5a631834e6f83e8359b571cfcbf5a18b04990288c8569cc16ee0798" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "0fe24de6d37ea5a7724c56f0bb01efcbb3fe999a6e461ec1392f3c3b105cc5ac" "28a104f642d09d3e5c62ce3464ea2c143b9130167282ea97ddcc3607b381823f" "2d035eb93f92384d11f18ed00930e5cc9964281915689fa035719cab71766a15" "9685cefcb4efd32520b899a34925c476e7920725c8d1f660e7336f37d6d95764" default))
+ '(cwm-centered-window-width 140)
+ '(cwm-frame-internal-border 45)
+ '(cwm-use-vertical-padding t)
+ '(default-input-method nil)
+ '(dictionary-port 2628)
+ '(dictionary-server "ssh.siyasang.com")
+ '(electric-pair-mode t)
+ '(exwm-floating-border-color "#888888")
+ '(fci-rule-color "#62686E" t)
+ '(fill-column 80)
+ '(forge-topic-list-limit '(100 . -100))
+ '(forge-topic-list-order '(created . string>))
+ '(global-display-line-numbers-mode t)
+ '(gnutls-algorithm-priority "normal:-vers-tls1.3")
+ '(highlight-tail-colors '(("#aecf90" . 0) ("#c0efff" . 20)))
+ '(hl-todo-keyword-faces
+   '(("HOLD" . "#70480f")
+     ("TODO" . "#721045")
+     ("NEXT" . "#5317ac")
+     ("THEM" . "#8f0075")
+     ("PROG" . "#00538b")
+     ("OKAY" . "#30517f")
+     ("DONT" . "#315b00")
+     ("FAIL" . "#a60000")
+     ("BUG" . "#a60000")
+     ("DONE" . "#005e00")
+     ("NOTE" . "#863927")
+     ("KLUDGE" . "#813e00")
+     ("HACK" . "#813e00")
+     ("TEMP" . "#5f0000")
+     ("FIXME" . "#a0132f")
+     ("XXX+" . "#972500")
+     ("REVIEW" . "#005a5f")
+     ("DEPRECATED" . "#201f55")))
+ '(ibuffer-deletion-face 'modus-themes-mark-del)
+ '(ibuffer-marked-face 'modus-themes-mark-sel)
+ '(ibuffer-title-face 'default)
+ '(ivy-posframe-min-width 165)
+ '(ivy-posframe-mode t nil (ivy-posframe))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1c1f24" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f24" "#7bc275"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f24" "#484854"))
+ '(jedi:complete-on-dot t)
+ '(jedi:environment-root "~/.emacs.d/elpa/jedi-core-20210503.1315" nil nil "This isn't the vriable to customize for managing virtualenv")
+ '(jedi:goto-definition-config
+   '((nil nil nil)
+     (t nil nil)
+     (nil definition nil)
+     (t definition nil)
+     (nil nil t)
+     (t nil t)
+     (nil definition t)
+     (t definition t)))
+ '(jedi:setup-keys t)
+ '(package-selected-packages
+   '(borland-blue-theme kubernetes-helm kubernetes ivy-rich ssh smartparens all-the-icons-gnus eyebrowse poetry unicode-fonts lsp-treemacs lsp-ivy lsp-ui company-posframe orderless corfu lsp-mode forge closql ghub editorconfig-generate editorconfig magit minimap posframe treepy ace-window all-the-icons avy back-button button-lock cfrs company compat concurrent ctable dash deferred dired-hacks-utils edit-indirect emacsql epc epl f geiser git-commit goto-chg ht hydra ivy jedi-core list-utils load-env-vars lv markdown-mode nav-flash org pcache persistent-soft persp-mode pfuture pkg-info powerline projectile python-environment pyvenv reformatter rust-mode s smartrep spinner string-utils svg-lib swiper transient vterm with-editor xterm-color yaml yasnippet treemacs evil yaml-mode geiser-guile codespaces git-link blamer org-superstar doom dockerfile-mode badwolf-theme restclient treemacs-persp multiple-cursors centered-window org-rainbow-tags ob-git-permalink org-appear sqlformat ivy-posframe diredfl dired-narrow drag-stuff circadian expand-region peg eshell-syntax-highlighting dashboard hemera-theme angular-mode separedit standard-themes dired-rainbow darkroom fixmee bicycle nyx-theme rustic git-gutter go-mode focus minimal-theme prettier-js highlight-symbol emmet-mode all-the-icons-dired yaml-pro fira-code-mode nyan-mode dired-subtree ini tao-theme graphql treemacs-icons-dired company-jedi better-defaults melancholy-theme disable-mouse org-bullets doom-themes centaur-tabs ztree sql-clickhouse hima-theme multi-vterm rainbow-delimiters graphql-mode treemacs-projectile spaceline ibuffer-vc paredit highlight-indent-guides modus-themes wc-mode pipenv counsel which-key yasnippet-classic-snippets svg-tag-mode py-isort vs-dark-theme nerd-icons))
+ '(safe-local-variable-values
+   '((eval let
+           ((venv-path
+             (expand-file-name "backend/.venv"
+                               (locate-dominating-file default-directory ".dir-locals.el"))))
+           (when
+               (file-exists-p venv-path)
+             (pyvenv-activate venv-path)))
+     (eval let
+           ((venv-path
+             (expand-file-name ".venv"
+                               (locate-dominating-file default-directory ".dir-locals.el"))))
+           (when
+               (file-exists-p venv-path)
+             (pyvenv-activate venv-path)))
+     (eval progn
+           (pipenv-activate))
+     (eval progn
+           (pipenv-activate)
+           (setq jedi:environment-root python-shell-virtualenv-root))
+     (eval progn
+           (pipenv-activate t))
+     (encoding . UTF-8)))
+ '(send-mail-function 'smtpmail-send-it))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fringe ((t (:background "#ffffff"))))
+ '(helm-posframe-border ((t (:background "#6272a4"))))
+ '(ivy-posframe-border ((t (:background "#6272a4"))))
+ '(jedi:highlight-function-argument ((t (:inherit bold :underline t :slant italic)))))
