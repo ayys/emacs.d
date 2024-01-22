706624d76a1d8cc4edba74ab8bbf79d4cce7d9d0
Ayush Jha <ayush@wasmer.io>
2024-01-22 14:01:47 +0545

    add navapali-maps

---
+++ navapali-maps.el
@@ -0,0 +1,77 @@
+(define-key key-translation-map (kbd "C-ऽ") (kbd "C-a"))
+(define-key key-translation-map (kbd "M-ऽ") (kbd "M-a"))
+
+(define-key key-translation-map (kbd "C-ब") (kbd "C-b"))
+(define-key key-translation-map (kbd "M-ब") (kbd "M-b"))
+
+(define-key key-translation-map (kbd "C-च") (kbd "C-c"))
+(define-key key-translation-map (kbd "M-च") (kbd "M-c"))
+
+(define-key key-translation-map (kbd "C-द") (kbd "C-d"))
+(define-key key-translation-map (kbd "M-द") (kbd "M-d"))
+
+(define-key key-translation-map (kbd "C-ए") (kbd "C-e"))
+(define-key key-translation-map (kbd "M-ए") (kbd "M-e"))
+
+(define-key key-translation-map (kbd "C-ड") (kbd "C-f"))
+(define-key key-translation-map (kbd "M-ड") (kbd "M-f"))
+
+(define-key key-translation-map (kbd "C-ग") (kbd "C-g"))
+(define-key key-translation-map (kbd "M-ग") (kbd "M-g"))
+
+(define-key key-translation-map (kbd "C-ह") (kbd "C-h"))
+(define-key key-translation-map (kbd "M-ह") (kbd "M-h"))
+
+(define-key key-translation-map (kbd "C-इ") (kbd "C-i"))
+(define-key key-translation-map (kbd "M-इ") (kbd "M-i"))
+
+(define-key key-translation-map (kbd "C-ज") (kbd "C-j"))
+(define-key key-translation-map (kbd "M-ज") (kbd "M-j"))
+
+(define-key key-translation-map (kbd "C-क") (kbd "C-k"))
+(define-key key-translation-map (kbd "M-क") (kbd "M-k"))
+
+(define-key key-translation-map (kbd "C-ल") (kbd "C-l"))
+(define-key key-translation-map (kbd "M-ल") (kbd "M-l"))
+
+(define-key key-translation-map (kbd "C-म") (kbd "C-m"))
+(define-key key-translation-map (kbd "M-म") (kbd "M-m"))
+
+(define-key key-translation-map (kbd "C-न") (kbd "C-n"))
+(define-key key-translation-map (kbd "M-न") (kbd "M-n"))
+
+(define-key key-translation-map (kbd "C-o") (kbd "C-o"))
+(define-key key-translation-map (kbd "M-o") (kbd "M-o"))
+
+(define-key key-translation-map (kbd "C-प") (kbd "C-p"))
+(define-key key-translation-map (kbd "M-प") (kbd "M-p"))
+
+(define-key key-translation-map (kbd "C-า") (kbd "C-q"))
+(define-key key-translation-map (kbd "M-า") (kbd "M-q"))
+
+(define-key key-translation-map (kbd "C-र") (kbd "C-r"))
+(define-key key-translation-map (kbd "M-र") (kbd "M-r"))
+
+(define-key key-translation-map (kbd "C-स") (kbd "C-s"))
+(define-key key-translation-map (kbd "M-स") (kbd "M-s"))
+
+(define-key key-translation-map (kbd "C-त") (kbd "C-t"))
+(define-key key-translation-map (kbd "M-त") (kbd "M-t"))
+
+(define-key key-translation-map (kbd "C-उ") (kbd "C-u"))
+(define-key key-translation-map (kbd "M-उ") (kbd "M-u"))
+
+(define-key key-translation-map (kbd "C-व") (kbd "C-v"))
+(define-key key-translation-map (kbd "M-व") (kbd "M-v"))
+
+(define-key key-translation-map (kbd "C-ङ") (kbd "C-w"))
+(define-key key-translation-map (kbd "M-ङ") (kbd "M-w"))
+
+(define-key key-translation-map (kbd "C-ट") (kbd "C-x"))
+(define-key key-translation-map (kbd "M-ट") (kbd "M-x"))
+
+(define-key key-translation-map (kbd "C-य") (kbd "C-y"))
+(define-key key-translation-map (kbd "M-य") (kbd "M-y"))
+
+(define-key key-translation-map (kbd "C-श") (kbd "C-z"))
+(define-key key-translation-map (kbd "M-श") (kbd "M-z"))
