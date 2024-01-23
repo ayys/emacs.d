59685bfbad747c2058cb11909d898157c094247e
Ayush Jha <ayush@wasmer.io>
2024-01-23 13:20:14 +0545

    added custom.el

--- custom.el
+++ custom.el
@@ -13,7 +13,57 @@
  '(centered-window-mode nil)
  '(column-number-mode t)
  '(custom-safe-themes
-   '("cc0ea29f3e80c5c622cea5fd83686dd36963a9abcde5b21dfe6cee653b67d72f" "46ae6660834278f41a1c074b817bb316f2da0196513b59a83a2a49b846a9921f" "ccb2ff53e9794d059ff941fabcf265b67c8418da664db8c4d6a3d656962b7135" "eb50f36ed5141c3f702f59baa1968494dc8e9bd22ed99d2aaa536c613c8782db" "4320a92406c5015e8cba1e581a88f058765f7400cf5d885a3aa9b7b9fc448fa7" "d0f70691dd3c066f8030e32ff842550365c0444f0203fbbdb2dd3a4ea47ffba4" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "a138ec18a6b926ea9d66e61aac28f5ce99739cf38566876dc31e29ec8757f6e2" "ce4234c32262924c1d2f43e6b61312634938777071f1129c7cde3ebd4a3028da" "016f665c0dd5f76f8404124482a0b13a573d17e92ff4eb36a66b409f4d1da410" "4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" "e963debb6066f1b4a6bdd24b2f4bf0ba4496c5ab7ecced9debfd4d1a512d94d1" "dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "500244baf96301754cf9f388e33115405b9d75f561dd535c0a26f97a5eab52c8" "8b930a6af47e826c12be96de5c28f1d142dccab1927f196589dafffad0fc9652" "4bf6e7eb89e40ff5ae016bfc8178acc98d59de12d142191b370f551da24d77ac" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "a589c43f8dd8761075a2d6b8d069fc985660e731ae26f6eddef7068fece8a414" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee" "ae426fc51c58ade49774264c17e666ea7f681d8cae62570630539be3d06fd964" "dc11cee30927281fe3f5c77372119d639e77e86aa794dce2a6ff019afdfbec9e" "f4cdc8dea941e3c7e92b907e62cdc03e0483a350b738a43c2e118ce6be9880a6" "d7d317a4a3629d91129493ed6caa795c38e3b120da4df0156aa7700e9fe7d88c" "bdf2b5b617ea284bf2635f28509b3a5ce3fb1ed82cac00196a71cd8403fa3e60" "830596655dc39879096d9b7772768de6042fb5a4293c6b90c98a9b98bce96e4a" "e33efadcb0406fc9b0f0fd78e3afbfdb2825d9a9796b8d6671fad6bf38427bd1" "d4a02820fada527bad6414f1ba432d87155fa627f3111a226018885d3127f093" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "e27c391095dcee30face81de5c8354afb2fbe69143e1129109a16d17871fc055" "88deeaaab5a631834e6f83e8359b571cfcbf5a18b04990288c8569cc16ee0798" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "0fe24de6d37ea5a7724c56f0bb01efcbb3fe999a6e461ec1392f3c3b105cc5ac" "28a104f642d09d3e5c62ce3464ea2c143b9130167282ea97ddcc3607b381823f" "2d035eb93f92384d11f18ed00930e5cc9964281915689fa035719cab71766a15" "9685cefcb4efd32520b899a34925c476e7920725c8d1f660e7336f37d6d95764" default))
+   '("d0fd069415ef23ccc21ccb0e54d93bdbb996a6cce48ffce7f810826bb243502c"
+     "10e5d4cc0f67ed5cafac0f4252093d2119ee8b8cb449e7053273453c1a1eb7cc"
+     "88267200889975d801f6c667128301af0bc183f3450c4b86138bfb23e8a78fb1"
+     "0340489fa0ccbfa05661bc5c8c19ee0ff95ab1d727e4cc28089b282d30df8fc8"
+     "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0"
+     "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
+     "ffba0482d3548c9494e84c1324d527f73ea4e43fff8dfd0e48faa8fc6d5c2bc7"
+     "c3957b559cf3606c9a40777c5712671db3c7538e5d5ea9f63eb0729afeac832b"
+     "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02"
+     "50bb891011dfe0c30cd463c65e898523788d4ac4e6df141eed75030a33da1135"
+     "cc0ea29f3e80c5c622cea5fd83686dd36963a9abcde5b21dfe6cee653b67d72f"
+     "46ae6660834278f41a1c074b817bb316f2da0196513b59a83a2a49b846a9921f"
+     "ccb2ff53e9794d059ff941fabcf265b67c8418da664db8c4d6a3d656962b7135"
+     "eb50f36ed5141c3f702f59baa1968494dc8e9bd22ed99d2aaa536c613c8782db"
+     "4320a92406c5015e8cba1e581a88f058765f7400cf5d885a3aa9b7b9fc448fa7"
+     "d0f70691dd3c066f8030e32ff842550365c0444f0203fbbdb2dd3a4ea47ffba4"
+     "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb"
+     "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "a138ec18a6b926ea9d66e61aac28f5ce99739cf38566876dc31e29ec8757f6e2"
+     "ce4234c32262924c1d2f43e6b61312634938777071f1129c7cde3ebd4a3028da"
+     "016f665c0dd5f76f8404124482a0b13a573d17e92ff4eb36a66b409f4d1da410"
+     "4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9"
+     "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b"
+     "3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767"
+     "e963debb6066f1b4a6bdd24b2f4bf0ba4496c5ab7ecced9debfd4d1a512d94d1"
+     "dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5"
+     "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2"
+     "500244baf96301754cf9f388e33115405b9d75f561dd535c0a26f97a5eab52c8"
+     "8b930a6af47e826c12be96de5c28f1d142dccab1927f196589dafffad0fc9652"
+     "4bf6e7eb89e40ff5ae016bfc8178acc98d59de12d142191b370f551da24d77ac"
+     "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476"
+     "a589c43f8dd8761075a2d6b8d069fc985660e731ae26f6eddef7068fece8a414"
+     "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef"
+     "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52"
+     "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee"
+     "ae426fc51c58ade49774264c17e666ea7f681d8cae62570630539be3d06fd964"
+     "dc11cee30927281fe3f5c77372119d639e77e86aa794dce2a6ff019afdfbec9e"
+     "f4cdc8dea941e3c7e92b907e62cdc03e0483a350b738a43c2e118ce6be9880a6"
+     "d7d317a4a3629d91129493ed6caa795c38e3b120da4df0156aa7700e9fe7d88c"
+     "bdf2b5b617ea284bf2635f28509b3a5ce3fb1ed82cac00196a71cd8403fa3e60"
+     "830596655dc39879096d9b7772768de6042fb5a4293c6b90c98a9b98bce96e4a"
+     "e33efadcb0406fc9b0f0fd78e3afbfdb2825d9a9796b8d6671fad6bf38427bd1"
+     "d4a02820fada527bad6414f1ba432d87155fa627f3111a226018885d3127f093"
+     "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7"
+     "e27c391095dcee30face81de5c8354afb2fbe69143e1129109a16d17871fc055"
+     "88deeaaab5a631834e6f83e8359b571cfcbf5a18b04990288c8569cc16ee0798"
+     "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6"
+     "0fe24de6d37ea5a7724c56f0bb01efcbb3fe999a6e461ec1392f3c3b105cc5ac"
+     "28a104f642d09d3e5c62ce3464ea2c143b9130167282ea97ddcc3607b381823f"
+     "2d035eb93f92384d11f18ed00930e5cc9964281915689fa035719cab71766a15"
+     "9685cefcb4efd32520b899a34925c476e7920725c8d1f660e7336f37d6d95764" default))
  '(cwm-centered-window-width 140)
  '(cwm-frame-internal-border 45)
  '(cwm-use-vertical-padding t)
@@ -32,24 +82,12 @@
  '(gnutls-algorithm-priority "normal:-vers-tls1.3")
  '(highlight-tail-colors '(("#aecf90" . 0) ("#c0efff" . 20)))
  '(hl-todo-keyword-faces
-   '(("HOLD" . "#70480f")
-     ("TODO" . "#721045")
-     ("NEXT" . "#5317ac")
-     ("THEM" . "#8f0075")
-     ("PROG" . "#00538b")
-     ("OKAY" . "#30517f")
-     ("DONT" . "#315b00")
-     ("FAIL" . "#a60000")
-     ("BUG" . "#a60000")
-     ("DONE" . "#005e00")
-     ("NOTE" . "#863927")
-     ("KLUDGE" . "#813e00")
-     ("HACK" . "#813e00")
-     ("TEMP" . "#5f0000")
-     ("FIXME" . "#a0132f")
-     ("XXX+" . "#972500")
-     ("REVIEW" . "#005a5f")
-     ("DEPRECATED" . "#201f55")))
+   '(("HOLD" . "#70480f") ("TODO" . "#721045") ("NEXT" . "#5317ac")
+     ("THEM" . "#8f0075") ("PROG" . "#00538b") ("OKAY" . "#30517f")
+     ("DONT" . "#315b00") ("FAIL" . "#a60000") ("BUG" . "#a60000")
+     ("DONE" . "#005e00") ("NOTE" . "#863927") ("KLUDGE" . "#813e00")
+     ("HACK" . "#813e00") ("TEMP" . "#5f0000") ("FIXME" . "#a0132f")
+     ("XXX+" . "#972500") ("REVIEW" . "#005a5f") ("DEPRECATED" . "#201f55")))
  '(ibuffer-deletion-face 'modus-themes-mark-del)
  '(ibuffer-marked-face 'modus-themes-mark-sel)
  '(ibuffer-title-face 'default)
@@ -61,48 +99,90 @@
  '(jedi:complete-on-dot t)
  '(jedi:environment-root "~/.emacs.d/elpa/jedi-core-20210503.1315" nil nil "This isn't the vriable to customize for managing virtualenv")
  '(jedi:goto-definition-config
-   '((nil nil nil)
-     (t nil nil)
-     (nil definition nil)
-     (t definition nil)
-     (nil nil t)
-     (t nil t)
-     (nil definition t)
-     (t definition t)))
+   '((nil nil nil) (t nil nil) (nil definition nil) (t definition nil) (nil nil t)
+     (t nil t) (nil definition t) (t definition t)))
  '(jedi:setup-keys t)
+ '(lsp-pyls-plugins-pyflakes-enabled nil)
+ '(lsp-pyls-server-command '("/opt/homebrew/bin/pyls"))
  '(lsp-pylsp-plugins-flake8-max-line-length 150)
  '(lsp-pylsp-plugins-pydocstyle-add-ignore ["D1"])
  '(lsp-pylsp-plugins-pydocstyle-ignore ["D1"])
+ '(lsp-pylsp-server-command '("/opt/homebrew/bin/pylsp"))
+ '(lsp-ruff-lsp-server-command '("/opt/homebrew/bin/ruff-lsp"))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files
+   '("/Users/ayushjha/docs/org/गऽपऽकए/दoहoरइनए.oरग"
+     "/Users/ayushjha/docs/org/गऽपऽकए/नउननऽ-परऽ.oरग"
+     "/Users/ayushjha/docs/org/गऽपऽकए/परऽतहऽम-परऽ.oरग"
+     "/Users/ayushjha/docs/org/गऽपऽकए/मऽदहयऽ-परऽ.oरग"))
  '(package-selected-packages
-   '(transient-extras restclient-helm web-mode tree-sitter-langs tree-sitter realgud difftastic elpher almost-mono-themes dirvish magit-todos ef-themes doom-themes ewal-spacemacs-themes git-link nasm-mode yasnippet-snippets lsp-python-ms catppuccin-theme deft graphql graphql-doc graphql-mode borland-blue-theme kubernetes-helm kubernetes ivy-rich ssh smartparens all-the-icons-gnus eyebrowse poetry unicode-fonts lsp-treemacs lsp-ivy lsp-ui company-posframe orderless corfu lsp-mode forge closql editorconfig-generate editorconfig magit minimap posframe treepy ace-window all-the-icons avy back-button button-lock cfrs company compat concurrent ctable dash deferred dired-hacks-utils edit-indirect emacsql epc epl f geiser git-commit goto-chg ht hydra ivy jedi-core list-utils load-env-vars lv markdown-mode nav-flash org pcache persistent-soft persp-mode pfuture pkg-info powerline projectile python-environment pyvenv reformatter rust-mode s smartrep spinner string-utils svg-lib swiper transient vterm with-editor xterm-color yaml yasnippet treemacs evil yaml-mode geiser-guile codespaces blamer org-superstar doom dockerfile-mode badwolf-theme restclient treemacs-persp multiple-cursors centered-window org-rainbow-tags ob-git-permalink org-appear sqlformat ivy-posframe diredfl dired-narrow drag-stuff circadian expand-region peg eshell-syntax-highlighting dashboard hemera-theme angular-mode separedit standard-themes dired-rainbow darkroom fixmee bicycle nyx-theme rustic git-gutter go-mode focus minimal-theme prettier-js highlight-symbol emmet-mode all-the-icons-dired yaml-pro fira-code-mode nyan-mode dired-subtree ini tao-theme treemacs-icons-dired company-jedi better-defaults melancholy-theme disable-mouse org-bullets centaur-tabs ztree sql-clickhouse hima-theme multi-vterm rainbow-delimiters treemacs-projectile spaceline ibuffer-vc paredit highlight-indent-guides modus-themes wc-mode pipenv counsel which-key yasnippet-classic-snippets svg-tag-mode py-isort vs-dark-theme nerd-icons))
+   '(ace-window all-the-icons all-the-icons-completion all-the-icons-dired
+                all-the-icons-gnus almost-mono-themes angular-mode astro-ts-mode
+                avy back-button badwolf-theme better-defaults bicycle blamer
+                borland-blue-theme button-lock catppuccin-theme centaur-tabs
+                centered-window cfrs circadian closql codespaces company
+                company-jedi company-posframe compat concurrent consult corfu
+                ctable dap-mode darkroom dash dashboard deferred deft difftastic
+                diminish dired-hacks-utils dired-narrow dired-rainbow
+                dired-subtree diredfl dirvish disable-mouse dockerfile-mode doom
+                doom-themes dotenv-mode drag-stuff edit-indirect editorconfig
+                editorconfig-generate ef-themes elpher emacsql emmet-mode epc
+                epl eshell-syntax-highlighting evil ewal-spacemacs-themes
+                expand-region eyebrowse f faces fira-code-mode fixmee
+                flymake-ruff focus font-lock forge geiser geiser-guile
+                git-commit git-gutter git-link go-mode goto-chg graphql
+                graphql-doc graphql-mode hemera-theme highlight-indent-guides
+                highlight-parentheses highlight-symbol hima-theme howm ht
+                htmlize hydra ibuffer-vc ini ivy-posframe jedi-core kubernetes
+                kubernetes-helm list-utils load-env-vars lsp-python-ms lv magit
+                magit-todos marginalia melancholy-theme minimal-theme minimap
+                modus-themes monochrome-theme multi-vterm multiple-cursors
+                nano-theme nasm-mode nav-flash nerd-icons nushell-mode nyan-mode
+                nyx-theme ob-git-permalink orderless org org-appear org-bullets
+                org-projectile org-rainbow-tags org-superstar package-build
+                package-utils paredit pcache peg persistent-soft persp-mode
+                pfuture pipenv pixel-scroll pkg-info poetry posframe powerline
+                prettier-js projectile py-isort python-environment python-mls
+                pyvenv quasi-monochrome-theme rainbow-delimiters realgud
+                reformatter restclient rg ruff-format rust-mode rustic s
+                scroll-on-jump separedit smartparens smartrep spaceline spinner
+                sql-clickhouse sqlformat ssh standard-themes string-utils
+                svg-lib svg-tag-mode tao-theme totp transient transient-extras
+                tree-sit tree-sitter tree-sitter-langs treemacs
+                treemacs-icons-dired treemacs-persp treemacs-projectile treepy
+                treesit treesit-auto unicode-fonts verona-mode vertico
+                vertico-buffer vertico-directory vertico-flat vertico-grid
+                vertico-mouse vertico-multiform vertico-quick vertico-repeat
+                vertico-reverse vertico-unobtrusive vs-dark-theme vterm wc-mode
+                web-mode which-key with-editor xterm-color yaml yaml-mode
+                yaml-pro yasnippet yasnippet-classic-snippets yasnippet-snippets
+                ztree))
+ '(projectile-fd-executable "/opt/homebrew/bin/fd")
  '(safe-local-variable-values
    '((pyvenv-activate . "~/git/wasmer/backend/backend/.venv")
      (pyvenv-workon . "~/git/wasmer/backend/backend/.venv")
      (eval let
            ((venv-path
              (expand-file-name "backend/.venv"
-                               (locate-dominating-file default-directory ".dir-locals.el"))))
-           (when
-               (file-exists-p venv-path)
-             (pyvenv-activate venv-path)))
+                               (locate-dominating-file default-directory
+                                                       ".dir-locals.el"))))
+           (when (file-exists-p venv-path) (pyvenv-activate venv-path)))
      (eval let
            ((venv-path
              (expand-file-name ".venv"
-                               (locate-dominating-file default-directory ".dir-locals.el"))))
-           (when
-               (file-exists-p venv-path)
-             (pyvenv-activate venv-path)))
-     (eval progn
-           (pipenv-activate))
-     (eval progn
-           (pipenv-activate)
+                               (locate-dominating-file default-directory
+                                                       ".dir-locals.el"))))
+           (when (file-exists-p venv-path) (pyvenv-activate venv-path)))
+     (eval progn (pipenv-activate))
+     (eval progn (pipenv-activate)
            (setq jedi:environment-root python-shell-virtualenv-root))
-     (eval progn
-           (pipenv-activate t))
-     (projectile-tags-extra-directories "/home/ays/.local/share/virtualenvs/demo-drf-knox-EolPL3G1")
-     (projectile-tags-extra-directories "/home/ays/.local/share/virtualenvs/src-bB_RK6bF")
-     (projectile-tags-extra-directories "/home/ays/.local/share/virtualenvs/src-bB_RK6bF/lib/python3.9/site-packages")
+     (eval progn (pipenv-activate t))
+     (projectile-tags-extra-directories
+      "/home/ays/.local/share/virtualenvs/demo-drf-knox-EolPL3G1")
+     (projectile-tags-extra-directories
+      "/home/ays/.local/share/virtualenvs/src-bB_RK6bF")
+     (projectile-tags-extra-directories
+      "/home/ays/.local/share/virtualenvs/src-bB_RK6bF/lib/python3.9/site-packages")
      (encoding . UTF-8)))
  '(send-mail-function 'smtpmail-send-it))
 (custom-set-faces
